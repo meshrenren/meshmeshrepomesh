e4fad6410b27ae882217af235a629ab0f9f4264b
Ren Mesh <renrin.ampan@gmail.com>
2017-11-05 13:16:34 +0800

    Add karik

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=coop2',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
