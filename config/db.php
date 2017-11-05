e1141aa15594138b178925f0f25b5713337a6951
Ren Mesh <renrin.ampan@gmail.com>
2017-11-05 13:51:21 +0800

    Remove ignore files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=coop2',
-    'username' => 'root',
-    'password' => 'd',
-    'charset' => 'utf8',
-];
