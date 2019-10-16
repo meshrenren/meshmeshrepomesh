a66d6609bdf680491cf9f2a1194373a4fd5f8a76
Ren Ampan <renrin.ampan@gmail.com>
2019-10-16 09:13:45 +0800

    Loan evaluation print

---
+++ resources/js/vue/components/General/DialogModal.vue
@@ -0,0 +1,30 @@
+<template>
+	<el-dialog @close="closeModal" :visible.sync="dialogVisible" :width = "width" top = "20px" >
+		<span slot="title" class="el-dialog__title" >{{ titleHeader }}</span>
+		<slot></slot>
+	</el-dialog>
+</template>
+
+<script>
+import {dialogComponent} from '../../mixins/dialogComponent.js'
+
+export default {
+    props: {
+    	titleHeader :{
+    		type : String, 
+    		deault : ""
+    	},
+    	width : {
+    		type : String, 
+    		deault : "40%"
+    	}
+    },
+    data(){
+    	return {
+			dialogVisible		: true,
+    	}
+    },
+    mixins: [dialogComponent],
+}
+</script>
+
---
+++ resources/js/vue/components/General/PrintHeader.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="print-header">
+		<table>
+            <tr>
+                <td>
+                      <img style="width:  65px" :src = "$baseUrl+'/images/coop_logo.png'" />
+                </td>
+                <td style="padding: 0px 10px;">
+                    <h3 style="font-weight: bold; font-size: 16px; margin-bottom: 0px;">
+                        DILG XI EMPLOYEES MULTI-PURPOSE COOPERATIVE
+                    </h3>
+                    <p>DILG XI Regional Office, McArthur Highway, Matinam Davao City <br>
+                            Tel #: 297-27-01 / 297-26-06 / 297-26-04
+                    </p>
+                </td>
+            </tr>
+        </table>
+	</div>
+</template>
+<script>
+
+export default {
+	data: function () {
+		return{
+		}
+	},
+	created(){
+		
+	},
+}
+
+</script>
