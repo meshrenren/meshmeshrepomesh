c0cfda5885b4fcfac99cdc6afab6bee98c4fe53b
harold <roldify27@gmail.com>
2020-08-25 22:19:30 +0800

    updated printing of loan evaluation

--- resources/js/vue/components/Loan/LoanEvaluationForm.vue
+++ resources/js/vue/components/Loan/LoanEvaluationForm.vue
@@ -27,21 +27,21 @@
 	        			<tr>
 	        				<th>Share Capital: </th>
 	        				<td>
-	        					<span v-if = "shareAccount">{{ $nf.formatNumber(shareAccount.balance) }}</span>
+	        					<span v-if = "shareAccount">{{ $nf.formatNumber(shareAccount.balance, 2) }}</span>
 	        				</td>
 	        			</tr>
 	        			<tr>
 	        				<th>Maximum Loan Amount: </th>
 	        				<td>
-	        					<span v-if = "shareAccount">{{ $nf.formatNumber(shareAccount.max_amount) }}</span>
+	        					<span v-if = "shareAccount">{{ $nf.formatNumber(shareAccount.max_amount, 2) }}</span>
 	        				</td>
 	        			</tr>
 	        		</table>
 	        	</div>
 	        </div>
 	        <div class = "row">
-	        	<div class = "col-md-8" style = "margin-top: 20px;">
-	        		<table style="min-width: 400px;">
+	        	<div class = "col-md-12" style = "margin-top: 20px;">
+	        		<table class="centertable" style="min-width: 400px;">
 						<tr>
 		        			<th>
 		        				<h3>Debit</h3>
@@ -55,49 +55,49 @@
 
 		        		<tr>
 		        			<td>
-		        				<span>{{ $nf.formatNumber(evaluationForm.debit_loan) }}</span>
+		        				<span>{{ $nf.formatNumber(evaluationForm.debit_loan, 2) }}</span>
 						  	</td>
 						  	<th>
 		        				<span class = "label-title">LOAN</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.credit_loan) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.credit_loan, 2) }}</span>
 						  	</td>
 						</tr>
 
 						<tr>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.debit_interest) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.debit_interest, 2) }}</span>
 						  	</td>
 						  	<th>
 		        				<span class = "label-title">Interest</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.credit_interest) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.credit_interest, 2) }}</span>
 						  	</td>
 						</tr>
 
 						<tr>
 							<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.debit_preinterest) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.debit_preinterest, 2) }}</span>
 						  	</td>
 						  	<th>
 		        				<span class = "label-title">Prepaid Int</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.credit_preinterest) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.credit_preinterest, 2) }}</span>
 						  	</td>
 						</tr>
 
 						<tr>
 							<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.debit_redemption_ins) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.debit_redemption_ins, 2) }}</span>
 						  	</td>
 						  	<th>
 		        				<span class = "label-title">Redemp. Ins.</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.credit_redemption_ins) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.credit_redemption_ins, 2) }}</span>
 						  	</td>
 						</tr>
 						
@@ -109,7 +109,7 @@
 		        				<span class = "label-title">Service Charge</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.service_charge_amount) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.service_charge_amount, 2) }}</span>
 						  	</td>
 						</tr>
 
@@ -121,7 +121,7 @@
 		        				<span class = "label-title">Retention</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.savings_retention) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.savings_retention, 2) }}</span>
 						  	</td>
 						</tr>
 
@@ -134,7 +134,7 @@
 		        				<span class = "label-title">Notary</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.notary_amount) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.notary_amount, 2) }}</span>
 						  	</td>
 						</tr>
 
@@ -154,13 +154,13 @@
 						</tr>
 						<tr>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.debit_total) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.debit_total, 2) }}</span>
 						  	</td>
 						  	<th>
 		        				<span class = "label-title">TOTAL</span>
 						  	</th>
 						  	<td>
-						  		<span>{{ $nf.formatNumber(evaluationForm.credit_total) }}</span>
+						  		<span>{{ $nf.formatNumber(evaluationForm.credit_total, 2) }}</span>
 						  	</td>
 						</tr>
 					</table>
@@ -168,38 +168,38 @@
 	        </div>
 	        <div class = "row">
 	        	<div class = "col-md-12" style = "margin-top: 20px;">
-	        		<table style="text-align: left;">
+	        		<table class="centertable"  style="text-align: left;">
 	        			<tr>
 	        				<th>Loan Amount: </th>
 	        				<td>
-	        					<span>{{ $nf.formatNumber(evaluationForm.amount) }}</span>
+	        					<span>{{ $nf.formatNumber(evaluationForm.amount, 2) }}</span>
 	        				</td>
 	        				<td>
 	        					&nbsp;&nbsp;&nbsp;&nbsp;
 	        				</td>
 	        				<th>Previous Loan Amount: </th>
 	        				<td>
-	        					<span v-if = "latestLoan">{{ $nf.formatNumber(latestLoan.principal) }}</span>
+	        					<span v-if = "latestLoan">{{ $nf.formatNumber(latestLoan.principal, 2) }}</span>
 	        				</td>
 	        			</tr>
 	        			<tr>
 	        				<th>Payment/Quincena: </th>
 	        				<td>
-	        					<span>{{ $nf.formatNumber(evaluationForm.principal_amortization_quincena) }}</span>
+	        					<span>{{ $nf.formatNumber(evaluationForm.principal_amortization_quincena, 2) }}</span>
 	        				</td>
 	        				<td>
 	        					&nbsp;&nbsp;&nbsp;&nbsp;
 	        				</td>
 	        				<th>Previous Loan Balance: </th>
 	        				<td>
-	        					<span v-if = "latestLoan">{{ $nf.formatNumber(latestLoan.principal_balance) }}</span>
+	        					<span v-if = "latestLoan">{{ $nf.formatNumber(latestLoan.principal_balance, 2) }}</span>
 	        				</td>
 	        			</tr>
 	        			<tr>
 
 	        				<th>PI Payment/Quincena: </th>
 	        				<td>
-	        					<span>{{ $nf.formatNumber(evaluationForm.prepaid_amortization_quincena) }}</span>
+	        					<span>{{ $nf.formatNumber(evaluationForm.prepaid_amortization_quincena, 2) }}</span>
 	        				</td>
 	        				<td>
 	        					&nbsp;&nbsp;&nbsp;&nbsp;
@@ -212,14 +212,14 @@
 	        			<tr>
 	        				<th>Term: </th>
 	        				<td>
-	        					<span>{{ $nf.formatNumber(evaluationForm.duration) }}</span>
+	        					<span>{{ $nf.formatNumber(evaluationForm.duration, 2) }}</span>
 	        				</td>
 	        			</tr>
 	        		</table>
 	        	</div>
 	        	<div class = "col-md-12" style = "margin-top: 20px;">
 	        		<h3>OTHER LOANS:</h3>
-	        		<table class = "table table-bordered" style="text-align: left;">
+	        		<table class = "table table-borderedx " style="text-align: left;">
 	        			<thead>
 	        				<tr>
 		        				<th>Loan Type </th>
@@ -244,7 +244,7 @@
 
 	        	<div class = "col-md-12" style = "margin-top: 20px;">
 	        		<h3>Payments:</h3>
-	        		<table class = "table table-bordered" style="text-align: left;">
+	        		<table class = "table table-borderedx" style="text-align: left;">
 	        			<thead>
 	        				<tr>
 		        				<th>DateType </th>
@@ -258,7 +258,7 @@
 	        				<tr v-for = "trans in loanTransaction">
 	        					<td>{{ trans.date_posted}}</td>
 	        					<td>{{ trans.OR_no }}</td>
-	        					<td>{{ $nf.formatNumber(trans.principal_paid, 2) }}</td>
+	        					<td class="centerno">{{ $nf.formatNumber(trans.principal_paid, 2) }}</td>
 	        					<td>{{ $nf.formatNumber(trans.running_balance, 2) }}</td>
 	        					<td>{{ trans.remarks }}</td>
 	        				</tr>
@@ -266,7 +266,30 @@
 	        			
 	        		</table>
 	        	</div>
+
+
+				
+
 	        </div>
+
+			<div class="row">
+				<div class = "col-md-8" style = "margin-top: 20px;">
+				</div>
+				<div class = "col-md-4" style = "margin-top: 20px;">
+	        		<table style="width:100%; margin-top:10px;">
+						<tr>
+							<td><strong>Approved by:</strong></td>
+											
+						</tr>
+						<tr>
+							<td style="border-bottom:1px solid black; text-align:center;" >Charito L. Cinco</td> 
+						</tr>
+						<tr>
+							<td>Queen of DILG -> Pakiedit ko ani :D</td> 
+						</tr>
+					</table>	
+	        	</div>
+			</div>
         </div>
         <div>
         	<el-button type = "default" @click = "printPage()" ref = "print">Print</el-button> 
@@ -293,9 +316,11 @@ var printOptions =  {
     	'scrollbars=yes'
   	],
   	styles: [
-    	//"https://unpkg.com/kidlat-css/css/kidlat.css",
+		//"https://unpkg.com/kidlat-css/css/kidlat.css",		
     	window.Yii.baseUrl + "/css/bootstrap.min.css",
-    	window.Yii.baseUrl + "/css/mpdf.css"
+		window.Yii.baseUrl + "/css/mpdf.css",
+		window.Yii.baseUrl + "/css/assets/site.scss",
+		
   	]
 }
 console.log('printOptions', printOptions)
@@ -340,14 +365,14 @@ export default {
 		if(this.toPrint){
 			setTimeout(() => {
                 this.printPage()
-            }, 500);
+            }, 500);  
 		}
 	},
 	methods: {
 		printPage(){
 			this.$htmlToPaper('printPageContent', () => {
 			  	this.$EventDispatcher.fire('CLOSE_LOAN_EVALUATION_FORM')
-			});
+			}); 
 
 			
 		},
@@ -358,18 +383,7 @@ export default {
 <style lang="scss">
 	@import '../../assets/mpdf.scss'; 
   	@import '../../assets/site.scss';
-  	@import '~noty/src/noty.scss';
 
-  	.loan-evaluation-form{
-  		table {
-  			tr {
-  				td{
-  					padding: 0px 10px;
-  				}
-  			}
-  			
-  		}
 
-		
-	}
+  
 </style>
